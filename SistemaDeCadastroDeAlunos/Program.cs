11da79a95bf8fb134b4c9f4cf61a71132690a36a
Maaluu21 <marialuizapaes22@gmail.com>
2026-08-13 21:24:42 -0300

    feat: adiconando variaveis iniciais

--- SistemaDeCadastroDeAlunos/Program.cs
+++ SistemaDeCadastroDeAlunos/Program.cs
@@ -6,6 +6,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] nomes = new string[10];
+        int[] idades = new int[10];
+        decimal[] notas1 = new decimal[10];
+        decimal[] notas2 = new decimal[10];
+        int alunos = 0;
+
+
 
     }
 }
