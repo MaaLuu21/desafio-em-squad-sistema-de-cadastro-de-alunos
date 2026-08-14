722bd5d6537fa7aa498e144f31ff26c044addd49
usuario <usuario@supports-MacBook-Pro.local>
2026-08-13 22:27:24 -0300

    Criação do fluxo inicial de cadastro de alunos

--- SistemaDeCadastroDeAlunos/Program.cs
+++ SistemaDeCadastroDeAlunos/Program.cs
@@ -10,9 +10,42 @@ class Program
         int[] idades = new int[10];
         decimal[] notas1 = new decimal[10];
         decimal[] notas2 = new decimal[10];
+
         int alunos = 0;
 
+        Console.WriteLine("Cadastro de alunos");
 
+        for (alunos = 0; alunos < 10; alunos++)
+        {
+            Console.WriteLine($"Aluno {alunos + 1}");
 
+            Console.WriteLine("Nome: ");
+            nomes[alunos] = Console.ReadLine();
+
+            Console.WriteLine("Idade: ");
+            idades[alunos] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota 1: ");
+            notas1[alunos] = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota 2: ");
+            notas2[alunos] = decimal.Parse(Console.ReadLine());
+
+            alunos++;
+
+            if (alunos == 10)
+            {
+                Console.WriteLine("Limite de 10 alunos atingido.");
+                break; // menu principal
+            }
+
+            Console.WriteLine("Deseja cadastrar outro aluno? (S/N): ");
+            string resposta = Console.ReadLine().ToLower();
+
+            if (resposta != "s")
+            {
+                break; //menu principal
+            }
+        }
     }
 }
