c831cc4009da37a851add5f414edf1209fc5ddb4
usuario <usuario@supports-MacBook-Pro.local>
2026-08-16 17:57:16 -0300

    Fluxo do menu principal

--- SistemaDeCadastroDeAlunos/Program.cs
+++ SistemaDeCadastroDeAlunos/Program.cs
@@ -10,42 +10,91 @@ class Program
         int[] idades = new int[10];
         decimal[] notas1 = new decimal[10];
         decimal[] notas2 = new decimal[10];
+        int quantidadeAlunos = 0;
 
-        int alunos = 0;
+        Console.WriteLine("Bem-vindo. Vamos Cadastrar até 10 Alunos!");
 
-        Console.WriteLine("Cadastro de alunos");
-
-        for (alunos = 0; alunos < 10; alunos++)
+        bool validador = false;
+        do
         {
-            Console.WriteLine($"Aluno {alunos + 1}");
-
-            Console.WriteLine("Nome: ");
-            nomes[alunos] = Console.ReadLine();
-
-            Console.WriteLine("Idade: ");
-            idades[alunos] = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Nota 1: ");
-            notas1[alunos] = decimal.Parse(Console.ReadLine());
-
-            Console.WriteLine("Nota 2: ");
-            notas2[alunos] = decimal.Parse(Console.ReadLine());
-
-            alunos++;
-
-            if (alunos == 10)
+            Console.WriteLine("Quantos alunos vamos cadastrar?");
+            if (!int.TryParse(Console.ReadLine(), out int alunosDigitado) || alunosDigitado < 1 || alunosDigitado > 10)
             {
-                Console.WriteLine("Limite de 10 alunos atingido.");
-                break; // menu principal
+                Console.WriteLine("Quantidade de alunos inválida.");
             }
-
-            Console.WriteLine("Deseja cadastrar outro aluno? (S/N): ");
-            string resposta = Console.ReadLine().ToLower();
-
-            if (resposta != "s")
+            else
             {
-                break; //menu principal
+                quantidadeAlunos = alunosDigitado;
+                validador = true;
             }
+        } while (!validador);
+
+        for (int i = 0; i < quantidadeAlunos; i++)
+        {
+            Console.WriteLine($"Digite o nome do aluno {i + 1}:");
+            nomes[i] = Console.ReadLine().ToUpper().Trim();
+            Console.WriteLine($"Digite a idade do aluno {i + 1}:");
+            if (!int.TryParse(Console.ReadLine(), out int idadeDigitada))
+            {
+                Console.WriteLine("Idade Inválida, digite um número inteiro");
+            }
+            idades[i] = idadeDigitada;
+
+            Console.WriteLine($"Digite a primeira nota do aluno {i + 1}:");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal nota1Digitada))
+            {
+                Console.WriteLine("Nota inválida, digite um número válido.");
+            }
+            notas1[i] = nota1Digitada;
+
+            Console.WriteLine($"Digite a segunda nota do aluno {i + 1}:");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal nota2Digitada))
+            {
+                Console.WriteLine("Nota inválida, digite um número válido");
+            }
+            notas2[i] = nota2Digitada;
         }
+
+        int opcao;
+
+        do
+        {
+            Console.WriteLine("Menu Principal. Escolha uma opção:");
+            Console.WriteLine("1 - Listar alunos");
+            Console.WriteLine("2 - Buscar aluno");
+            Console.WriteLine("3 - Exibir aprovados");
+            Console.WriteLine("4 - Exibir média da turma");
+            Console.WriteLine("0 - Encerrar");
+
+            Console.WriteLine("Opção: ");
+            if (!int.TryParse(Console.ReadLine(), out opcao))
+            {
+                Console.WriteLine("Opção inválida!");
+                continue;
+            }
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Listando alunos...");
+                    break;
+                case 2:
+                    Console.WriteLine("Digite o nome do aluno que deseja buscar: ");
+                    break;
+                case 3:
+                    Console.WriteLine("Exibindo alunos aprovados...");
+                    break;
+                case 4:
+                    Console.WriteLine("Exibindo média da turma...");
+                    break;
+                case 0:
+                    Console.WriteLine("Encerrando o programa...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
     }
 }
